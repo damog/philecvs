59b07b72c8d27d45cc57e64bd6ad8bb0273c920f
David Moreno <damog@damog.net>
2004-04-04 05:51:20 +0000

    Se hace la parte del checkout y la del update.

--- update.php
+++ update.php
@@ -7,6 +7,11 @@
  * en su sistema.                            *
  *********************************************/
 
+// update.php - herramienta de actualización para phile
+//  David Moreno Garza <damog@damog.net>
+//  2004
+
+$cvs;
 $fe = 0;
 $paths = array(
 	       '/usr/bin/wget',
@@ -23,14 +28,6 @@ for($a = 0; $a < 4; $a++) {
   }
 }
 
-/*
-$fr = fopen("php://stdin", "r");
-$input = fgets($fr, 255);
-$input = rtrim($input);
-fclose($fr);
-print $input;
-*/
-
 if($fe != 0) {
   print "\n";
   print "update.php - update tool for phile \n";
@@ -40,21 +37,34 @@ if($fe != 0) {
   print "         -gp path  Specify gunzip binary path \n";
   print "         -cp path  Specify cvs binary path \n";
 } else {
-  print "Press <ENTER> on CVS password input \n";
-  $cvs = exec("$paths[3] -z3 -d$cvsroot login");
-
-  if($cvs) {
-    print "You are logged in :-) \n";
-    $load = exec("$paths[3] -z3 -d$cvsroot co phile");
-    if($load) {
+  if(file_exists('phile')) {
+    print "It seems you have a previous phile installation, I'll update it \n";
+    $cvs = exec("$paths[3] update -dP phile");
+    if($cvs) {
       print "\n";
       print "Thanks for using phile - php file manager \n";
     } else {
-      print "ERROR: Couldn't retrieve CVS tree \n";
+      print "ERROR: Couldn't update CVS tree \n";
     }
   } else {
-    print "ERROR: Couldn't log in to CVS \n";
-    die();
+    
+    print "Press <ENTER> on CVS password input \n";
+    $cvs = exec("$paths[3] -z3 -d$cvsroot login");
+    
+    if($cvs) {
+      print "You are logged in :-) \n";
+      print "I'll checkout a new cvs tree \n";
+      $load = exec("$paths[3] -z3 -d$cvsroot co phile");
+      if($load) {
+	print "\n";
+	print "Thanks for using phile - php file manager \n";
+      } else {
+	print "ERROR: Couldn't retrieve CVS tree \n";
+      }
+    } else {
+      print "ERROR: Couldn't log in to CVS \n";
+      die();
+    }
   }
 }
 
